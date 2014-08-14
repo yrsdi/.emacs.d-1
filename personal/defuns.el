2c7bc4d98b4d9df019da90aef35b5262d90ebaa8
Travis Jeffery <tj@travisjeffery.com>
2014-08-14 17:39:54 -0500

    Use t/ prefix

--- personal/defuns.el
+++ personal/defuns.el
@@ -1,11 +1,13 @@
-(defun t-email ()
+(require 'projectile)
+
+(defun t/email ()
   (interactive)
   (with-current-buffer (get-buffer-create "*email*")
     (markdown-mode)
     (toggle-word-wrap)
     (pop-to-buffer (current-buffer))))
 
-(defun comment-eclipse ()
+(defun t/comment-eclipse ()
   (interactive)
   (let ((start (line-beginning-position))
         (end (line-end-position)))
@@ -20,9 +22,9 @@
                   (point))))
     (comment-or-uncomment-region start end)))
 
-(global-set-key (kbd "M-;") 'comment-eclipse)
+(global-set-key (kbd "M-;") 't/comment-eclipse)
 
-(defun t-word-count ()
+(defun t/word-count ()
   (interactive)
   (shell-command-on-region (point-min) (point-max) "wc -w"))
 
@@ -44,23 +46,23 @@
         (add-hook 'isearch-mode-hook 'isearch-set-initial-string)
         (isearch-forward regexp-p no-recursive-edit)))))
 
-
-(defun t-config ()
-  (interactive)
-  (find-file "~/.emacs.d/personal/config.el"))
-
-(defun t-newline-and-indent-up ()
+(defun t/newline-and-indent-up ()
   (interactive)
   (line-move -1)
   (end-of-line)
   (newline-and-indent))
 
-(defun t-org ()
+(defun t/find-org-file ()
   "Find a .org file to edit."
   (interactive)
   (ido-find-file-in-dir "~/org"))
 
-(defun t-only-buffer ()
+(defun t/find-config-file ()
+  "Find a personal config file to edit."
+  (interactive)
+  (ido-find-file-in-dir "~/.emacs.d/personal/"))
+
+(defun t/only-buffer ()
   (interactive)
   (mapc 'kill-buffer (cdr (buffer-list (current-buffer)))))
 
@@ -89,12 +91,12 @@
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
 
-(defun t-marked ()
+(defun t/marked ()
   "View the current buffer in Marked.app"
   (interactive)
   (shell-command (format "open -a Marked %s" (expand-file-name (buffer-file-name (current-buffer))))))
 
-(defun t-kill-line-save (&optional arg)
+(defun t/kill-line-save (&optional arg)
   (interactive "p")
   (save-excursion
     (copy-region-as-kill
@@ -103,18 +105,30 @@
               (end-of-visible-line))
             (point)))))
 
-(global-set-key (kbd "C-c C-k") 't-kill-line-save)
+(global-set-key (kbd "C-c C-k") 't/kill-line-save)
 
-(defun t-eshell (name)
+(defun t/eshell (name)
   (interactive "sName: ")
   (let ((eshell-buffer-name (format "*eshell: %s*" name)))
     (if (get-buffer eshell-buffer-name)
         (switch-to-buffer-other-window eshell-buffer-name)
       (eshell eshell-buffer-name))))
 
-(defun t-eshell-execute-previous-input ()
+(defun t/eshell-execute-previous-input ()
   (interactive)
   (save-excursion
     (switch-to-buffer-other-window eshell-buffer-name)
     (call-interactively 'eshell-previous-matching-input-from-input)
     (eshell-send-input)))
+
+(defun s/goto (repo)
+  (interactive "sREPO: ")
+  (let* ((segment-dir "~/dev/segmentio/")
+         (repo-dir (concat segment-dir repo))
+         (find-file-in-repo (lambda ()
+                              (let ((default-directory repo-dir))
+                                (helm-projectile)))))
+    (if (file-exists-p repo-dir)
+        (funcall find-file-in-repo)
+      (shell-command (format "cd %s; git clone git@github.com:segmentio/%s.git" segment-dir repo))
+      (funcall find-file-in-repo))))
