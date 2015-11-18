11eb896208d1fe7e5e0067ffbbb6616afa8f2b63
Travis Jeffery <tj@travisjeffery.com>
2015-11-18 00:48:10 -0500

    rename fns

--- personal/bindings.el
+++ personal/bindings.el
@@ -3,18 +3,16 @@
 (eval-after-load 'prelude-mode
   '(progn
      (define-key prelude-mode-map (kbd "C-c o") nil)
-     (define-key prelude-mode-map (kbd "C-c u") nil)
-     ))
-(define-prefix-command 't/open-map)
-(global-set-key (kbd "C-c o") 't/open-map)
+     (define-key prelude-mode-map (kbd "C-c u") nil)))
+
+(define-prefix-command 'tj/open-map)
+(global-set-key (kbd "C-c o") 'tj/open-map)
 (global-set-key (kbd "C-c o o") 'prelude-open-with)
 (global-set-key (kbd "C-c o f") 'helm-open-github-from-file)
 (global-set-key (kbd "C-c o c") 'helm-open-github-from-commit)
 (global-set-key (kbd "C-c o i") 'helm-open-github-from-issues)
 (global-set-key (kbd "C-c o p") 'helm-open-github-from-pull-requests)
 (global-set-key (kbd "C-c u") 'browse-url-at-point)
-
-(global-set-key (kbd "<C-return>") 't/newline-and-indent-up)
-
+(global-set-key (kbd "<C-return>") 'tj/newline-and-indent-up)
 (global-set-key (kbd "C-\\") 'er/expand-region)
 (global-set-key (kbd "C->") 'mc/mark-all-like-this-dwim)
--- personal/defuns.el
+++ personal/defuns.el
@@ -1,13 +1,13 @@
 (require 'projectile)
 
-(defun t/email ()
+(defun tj/email ()
   (interactive)
   (with-current-buffer (get-buffer-create "*email*")
     (markdown-mode)
     (toggle-word-wrap)
     (pop-to-buffer (current-buffer))))
 
-(defun t/comment-eclipse ()
+(defun tj/comment-eclipse ()
   (interactive)
   (let ((start (line-beginning-position))
         (end (line-end-position)))
@@ -22,9 +22,9 @@
                   (point))))
     (comment-or-uncomment-region start end)))
 
-(global-set-key (kbd "M-;") 't/comment-eclipse)
+(global-set-key (kbd "M-;") 'tj/comment-eclipse)
 
-(defun t/word-count ()
+(defun tj/word-count ()
   (interactive)
   (shell-command-on-region (point-min) (point-max) "wc -w"))
 
@@ -46,23 +46,23 @@
         (add-hook 'isearch-mode-hook 'isearch-set-initial-string)
         (isearch-forward regexp-p no-recursive-edit)))))
 
-(defun t/newline-and-indent-up ()
+(defun tj/newline-and-indent-up ()
   (interactive)
   (line-move -1)
   (end-of-line)
   (newline-and-indent))
 
-(defun t/find-org-file ()
+(defun tj/find-org-file ()
   "Find a .org file to edit."
   (interactive)
   (ido-find-file-in-dir "~/Dropbox/org"))
 
-(defun t/find-config-file ()
+(defun tj/find-config-file ()
   "Find a personal config file to edit."
   (interactive)
   (ido-find-file-in-dir "~/.emacs.d/personal/"))
 
-(defun t/only-buffer ()
+(defun tj/only-buffer ()
   (interactive)
   (mapc 'kill-buffer (cdr (buffer-list (current-buffer)))))
 
@@ -91,12 +91,12 @@
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
 
-(defun t/marked ()
+(defun tj/marked ()
   "View the current buffer in Marked.app"
   (interactive)
   (shell-command (format "open -a Marked %s" (expand-file-name (buffer-file-name (current-buffer))))))
 
-(defun t/kill-line-save (&optional arg)
+(defun tj/kill-line-save (&optional arg)
   (interactive "p")
   (save-excursion
     (copy-region-as-kill
@@ -105,54 +105,58 @@
               (end-of-visible-line))
             (point)))))
 
-(global-set-key (kbd "C-c C-k") 't/kill-line-save)
+(global-set-key (kbd "C-c C-k") 'tj/kill-line-save)
 
-(defun t/eshell (name)
+(defun tj/eshell (name)
   (interactive "sName: ")
   (let ((eshell-buffer-name (format "*eshell: %s*" name)))
     (if (get-buffer eshell-buffer-name)
         (switch-to-buffer-other-window eshell-buffer-name)
       (eshell eshell-buffer-name))))
 
-(defun t/eshell-execute-previous-input ()
+(defun tj/eshell-execute-previous-input ()
   (interactive)
   (save-excursion
     (switch-to-buffer-other-window eshell-buffer-name)
     (call-interactively 'eshell-previous-matching-input-from-input)
     (eshell-send-input)))
 
-(defun s/goto (repo)
-  (interactive "sREPO: ")
-  (let* ((segment-dir "~/dev/src/github.com/segmentio/")
-         (repo-dir (concat segment-dir repo))
-         (find-file-in-repo (lambda ()
-                              (let ((default-directory repo-dir))
-                                (helm-projectile)))))
+(defun tj-goto (repo)
+  "Go to or clone the given dev `repo'."
+  (interactive
+   (list
+    (ido-read-directory-name "Directory: " "~/dev/")))
+  (let* ((dev-dir "~/dev/")
+         (repo-dir repo))
     (if (file-exists-p repo-dir)
-        (funcall find-file-in-repo)
-      (shell-command (format "cd %s; git clone git@github.com:segmentio/%s.git" segment-dir repo))
-      (funcall find-file-in-repo))))
+        (projectile-find-file-in-directory repo)
+      (shell-command (format "cd %s; git clone git@github.com:travisjeffery/%s.git" dev-dir repo))
+      (projectile-find-file-in-directory repo))))
+(global-set-key (kbd "C-M-T") 'tj-goto)
 
-(defun t/delete-file-and-buffer ()
+(defun tj/delete-file-and-buffer ()
   "Delete the current file and kill the current buffer."
   (interactive)
   (delete-file (buffer-file-name (current-buffer)))
   (kill-buffer (current-buffer)))
+(global-set-key (kbd "C-c K") 'tj/delete-file-and-buffer)
 
-(global-set-key (kbd "C-c K") 't/delete-file-and-buffer)
-
-(defun t/github-open-file ()
+(defun tj/github-open-file ()
   "View the current file in a web browser on GitHub."
   (interactive)
   (let* ((root (helm-open-github--root-directory))
          (repo-path (file-relative-name (buffer-file-name) root)))
     (helm-open-github--from-file-action repo-path)))
-(global-set-key (kbd "C-c o F") 't/github-open-file)
+(global-set-key (kbd "C-c o F") 'tj/github-open-file)
 
-(defun t/projectile-ack (&optional args)
+(defun tj/projectile-ack (&optional args)
   "Ack in specified directory."
   (interactive)
   (let ((current-prefix-arg t))
     (call-interactively 'projectile-ack)))
-(add-hook 'projectile-mode-hook (lambda () (define-key projectile-mode-map (kbd "C-c p s a") 't/projectile-ack)))
-(add-hook 'projectile-mode-hook (lambda () (define-key projectile-mode-map (kbd "s-p s a") 't/projectile-ack)))
+
+(defun tj/projectile-mode-hook ()
+  "My hook for projectile."
+  (define-key projectile-mode-map (kbd "C-c p s a") #'tj/projectile-ack)
+  (define-key projectile-mode-map (kbd "s-p s a") #'tj/projectile-ack))
+(add-hook 'projectile-mode-hook 'tj/projectile-mode-hook)
