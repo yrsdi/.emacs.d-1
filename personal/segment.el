cf44d8e504455b451e3f18be608cb899ca88f826
Travis Jeffery <tj@travisjeffery.com>
2016-11-13 17:43:46 -0500

    remove bunch of files

--- personal/segment.el
+++
@@ -1,7 +0,0 @@
-(defun s-find-file ()
-  (interactive)
-  (ido-find-file-in-dir "~/dev/src/github.com/segmentio/"))
-
-(defun s-open-repo (repo)
-  (interactive "sRepo: ")
-  (browse-url (format "https://github.com/segmentio/%s" repo)))
